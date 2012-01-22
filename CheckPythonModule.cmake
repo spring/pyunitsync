14cfb8618d07edd7abbfb2606dc48cc0fdb6dfe0
Rene Milk <rene.milk@uni-muenster.de>
2012-01-22 23:32:25 +0100

    makes cmake tests more verbose

--- CheckPythonModule.cmake
+++ CheckPythonModule.cmake
@@ -4,6 +4,8 @@ macro(CheckPythonModule module)
 	execute_process( 
 		COMMAND 
 			${PYTHON_EXECUTABLE} "${CMAKE_CURRENT_SOURCE_DIR}/CheckPythonModule.py" "${module}"
+		WORKING_DIRECTORY
+			${CMAKE_CURRENT_SOURCE_DIR}
 		RESULT_VARIABLE 
 			python_${module}_RESULT_VAR
 		)
