8dfe0bcb867150c4d83e6f691e44dc7a55fda21c
abma <spring@abma.de>
2022-01-09 16:06:23 +0100

    more cmake-lint warnings

--- CheckPythonModule.cmake
+++ CheckPythonModule.cmake
@@ -1,15 +1,15 @@
 # check whether given python module exists
 
-macro(CheckPythonModule module)
-	execute_process( 
-		COMMAND 
+macro (CheckPythonModule module)
+	execute_process(
+		COMMAND
 			${PYTHON_EXECUTABLE} "${CMAKE_CURRENT_SOURCE_DIR}/CheckPythonModule.py" "${module}"
 		WORKING_DIRECTORY
 			${CMAKE_CURRENT_SOURCE_DIR}
-		RESULT_VARIABLE 
+		RESULT_VARIABLE
 			python_${module}_RESULT_VAR
 		)
 	if ( python_${module}_RESULT_VAR EQUAL 0 )
 		set( python_${module}_FOUND 1 )
 	endif ()
-endmacro()
+endmacro ()
